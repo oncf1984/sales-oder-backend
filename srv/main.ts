4e276b08d8ebd379d200dff32ee3fc5a97aeea0f
ortiz filho <ortiz.nogueira@mail.com>
2026-02-10 14:12:22 -0300

    chore: updt tsconfig with paths/alias

--- srv/main.ts
+++ srv/main.ts
@@ -1,3 +1,15 @@
+//import { Customer, Customers } from '@cds-models/sales';
+import { Customer, Customers } from '@models/sales';
+
+const cust: Customer = {
+    email : "ortiz.filho@castgroup.com.br",
+    lastName : "filho",
+    firstName : 'Ortiz',
+    id: '123456'
+};
+
+const customers: Customers = [ cust];
+
 const funcao = (variavel: string) => console.log(variavel);
 
 funcao('123');
