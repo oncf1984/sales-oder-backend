a1372d1e272cfc320b2c76d237adb8dec6b1cf98
ortiz filho <ortiz.nogueira@mail.com>
2026-02-10 15:14:29 -0300

    feat: add after customer read logic

--- srv/main.ts
+++ srv/main.ts
@@ -1,16 +1,29 @@
-//import { Customer, Customers } from '@cds-models/sales';
-import { Customer, Customers } from '@models/sales';
+// //import { Customer, Customers } from '@cds-models/sales';
+// import { Customer, Customers } from '@models/sales';
 
-const cust: Customer = {
-    email : "ortiz.filho@castgroup.com.br",
-    lastName : "filho",
-    firstName : 'Ortiz',
-    id: '123456'
-};
+// const cust: Customer = {
+//     email : "ortiz.filho@castgroup.com.br",
+//     lastName : "filho",
+//     firstName : 'Ortiz',
+//     id: '123456'
+// };
 
-const customers: Customers = [ cust];
+// const customers: Customers = [ cust];
 
-const funcao = (variavel: string) => console.log(variavel);
+// const funcao = (variavel: string) => console.log(variavel);
 
-funcao('123');
+// funcao('123');
 
+import { Service } from '@sap/cds';
+import { Customers } from '@models/sales';
+export default(service: Service ) =>{
+    service.after('READ', 'Customers', (results:Customers) => {
+      console.log(results.at(-1));
+
+      results.forEach(customer =>{
+        if (!customer.email?.includes('@')){
+            customer.email = `${customer.email}@hotmail.com`
+        }
+      })
+    });
+}
